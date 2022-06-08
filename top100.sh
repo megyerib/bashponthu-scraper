da414acea1c08f39d6d610d71db89f0aa0b72b0d
Balázs Megyeri <megyerib@users.noreply.github.com>
2022-06-08 21:53:40 +0200

    Top 100 átalakítható bot 100-zá
    Kezelem a negítív előjelet is just for fun

--- top100.sh
+++ top100.sh
@@ -8,11 +8,11 @@ outfile=out/top100.html
 cd $(dirname $0)
 cat $(ls $indir/* | sort -V) \
 	| grep '<div class="qline">' \
-	| grep -oE "[0-9]+ -&gt; \([0-9]+\)" \
-	| sed -E "s|([0-9]+) -&gt; \(([0-9]+)\)|\2 \1|" \
-	| sort -rV \
+	| grep -oE "[0-9]+ -&gt; \(-?[0-9]+\)" \
+	| sed -E "s|([0-9]+) -&gt; \((-?[0-9]+)\)|\2 \1|" \
+	| sort -rg \
 	| head -100 \
-	| sed -E "s|[0-9]+ ([0-9]+)|$indir/\1.html|" \
+	| sed -E "s|-?[0-9]+ ([0-9]+)|$indir/\1.html|" \
 	| xargs -i cat {} \
 	| sed "s|\*|\&ast;|" \
 	| ./htmlgen.sh \
